fcbb0a4364d074a22ede6c73a24e5715ec01e492
drmcastles <drmcast@yandex.ru>
2019-06-27 23:20:48 +0300

    last commit

--- calendar.rb
+++ calendar.rb
@@ -3,7 +3,7 @@ puts 'Введитете даты в формате: ДД.ММ.ГГГГ'
 #создаем массив с полученными данным и конвертируем их в integer
 day, month, year = gets.chomp.split('.').map(&:to_i)
 #проверка является ли год високосным
-leap_year = year % 4 == 0 && year % 100 != 0 || year % 400 ==0
+leap_year = year % 4 == 0 && year % 100 != 0 || year % 400 == 0
 last_days[1] = 29 if leap_year
 # делаем срез по полученному месяцу
 days_point = last_days.take(month - 1)
